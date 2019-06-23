78822b4602e13d0dc9f47fb4453a9ef467014045
acer <acer@acer-PC>
2019-06-23 13:12:39 +0530

    Completed Successfully

--- src/main/java/main/Question1.java
+++ src/main/java/main/Question1.java
@@ -1,6 +1,8 @@
 package main;
 import java.util.Scanner;
 
+import javax.print.attribute.standard.PrinterLocation;
+
 class Courses {
 	public String displayCourseDetails(String dept) {
 			
@@ -8,38 +10,44 @@ class Courses {
 		}
 }
 
-class ECE { //extend the parent class here 
+class ECE extends Courses { //extend the parent class here 
+	String e;
 	public String displayCourseDetails(String dept) {
 		// write or edit your code here 
-		String commonSubjects = "";//call the parent function after using inheritence
-		String subject1 = ""; //write the specific subjects name here
-		String subject2 = ""; //write the specific subjects name here
+		this.e=dept;
+		String commonSubjects = super.displayCourseDetails(e);//call the parent function after using inheritence
+		String subject1 = "Microprocessor"; //write the specific subjects name here
+		String subject2 = "Linear Integrated Circuits"; //write the specific subjects name here
 		
 		String specificSubjects = " specific subjects are:\n4. "+subject1+"\n5. "+subject2;
 		return commonSubjects+dept+specificSubjects;
 		
 	}
 }
-class Mechanical { //extend the parent class here 
+class Mechanical extends Courses { //extend the parent class here 
+	String m;
 	public String displayCourseDetails(String dept) {
 		// write or edit your code here 
-		String commonSubjects = "";//call the parent function after using inheritence
+		this.m=dept;
+		String commonSubjects = super.displayCourseDetails(m);//call the parent function after using inheritence
 		String subject1 = "Fluid Mechanics"; //write the specific subjects name here
 		String subject2 = "Thermodynamics"; //write the specific subjects name here
 		
-		String specificSubjects = " specific subjects are:\n4. "+subject1+"\n5. "+subject2;
+		String specificSubjects =" specific subjects are:\n4. "+subject1+"\n5. "+subject2;
 		return commonSubjects+dept+specificSubjects;	
 	
 	}
 
 }
-class CSE  { //extend the parent class here 
+class CSE extends Courses  { //extend the parent class here 
+	String c;
 	public String displayCourseDetails(String dept) {
 		
 		// write or edit your code here 
-		String commonSubjects = "";//call the parent function after using inheritence
-		String subject1 = ""; //write the specific subjects name here
-		String subject2 = ""; //write the specific subjects name here
+		this.c=dept;
+		String commonSubjects = super.displayCourseDetails(c);//call the parent function after using inheritence
+		String subject1 = "Network Theory"; //write the specific subjects name here
+		String subject2 = "Operating Systems"; //write the specific subjects name here
 		
 		String specificSubjects = " specific subjects are:\n4. "+subject1+"\n5. "+subject2;
 		return commonSubjects+dept+specificSubjects;		
@@ -59,21 +67,24 @@ public class Question1 {
 		{
 			//this case is or ECE
 			//Initialize the Class using appropriately taking the concept of inheritence and polymorphism in mind.
-			System.out.println("");//call the function of the class that you initialized
+			ECE ece = new ECE();
+			System.out.println(ece.displayCourseDetails("ECE"));//call the function of the class that you initialized
 			break;
 		}
 		case 2:
 		{
 			//this case is or Mechanical
 			//Initialize the Class using appropriately taking the concept of inheritence and polymorphism in mind.
-			System.out.println("");//call the function of the class that you initialized
+			Mechanical mech = new Mechanical();
+			System.out.println(mech.displayCourseDetails("Mechanical"));//call the function of the class that you initialized
 			break;
 		}
 		case 3:
 		{
 			//this case is or CSE
 			//Initialize the Class using appropriately taking the concept of inheritence and polymorphism in mind.
-			System.out.println("");//call the function of the class that you initialized
+			CSE cse = new CSE();
+			System.out.println(cse.displayCourseDetails("CSE"));//call the function of the class that you initialized
 			break;
 		}
 		default:
